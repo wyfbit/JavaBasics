14b5d32d11de5fec9840dac4526c5b16b6085db2
wyfbit <wyfncut@163.com>
2018-03-22 22:29:29 +0800

    Test1

--- ResourceCompetition/src/CannotSolve/EvenChecker.java
+++ ResourceCompetition/src/CannotSolve/EvenChecker.java
@@ -18,7 +18,6 @@ public class EvenChecker implements Runnable{
               int val = generator.next();
               if(val % 2 != 0) {
                    System.out.println(val + "not even!");
-                   generator.cancel();
               }
          }
     }
